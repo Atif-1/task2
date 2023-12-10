ddc57e88d84ce8430c4f3c3b889bd2934cd0bb35
Atif-1 <atif1712001@gmail.com>
2023-12-10 22:10:07 +0530

    Task13

--- dom.js
+++ dom.js
@@ -1,15 +1,19 @@
 var form=document.querySelector('#main');
+var list=document.querySelector('#list');
 
-form.addEventListener('submit',onSubmit);
 function onSubmit(e){
 	e.preventDefault();
 let n=document.querySelector('#name').value;
 let email=document.querySelector('#mail').value;
 let p=document.querySelector('#phone').value;
+let tfc=document.querySelector('#tfc').value;
+let tf=document.querySelector('#tf').value;
 const my_obj=new Object();
 my_obj.name=n;
 my_obj.email=email;
 my_obj.phone=p;
+my_obj.dateForCall=tfc;
+my_obj.timeForCall=tf;
 //adding key
 var key=email;
 //serializing
@@ -19,7 +23,30 @@ var key=email;
 	
 	//showing on screen
 	let li=document.createElement('li');
-	let text=document.createTextNode(n+' '+email+' '+p);
-	li.appendChild(text);
+	li.appendChild(document.createTextNode(n));
+	li.appendChild(document.createTextNode(email));
+	li.appendChild(document.createTextNode(p));
+	li.appendChild(document.createTextNode(tfc));
+	li.appendChild(document.createTextNode(tf));
 	list.appendChild(li);
+	//creating delete button
+	var delbtn=document.createElement('button');
+	delbtn.className='delete';
+	delbtn.appendChild(document.createTextNode('Delete'));
+	li.appendChild(delbtn);
+	//add event on delete button
+	delbtn.addEventListener('click',del);
+
+	function del(e){
+		e.preventDefault();
+		if(confirm('Remove this appointment?')){
+			let li=e.target.parentElement;
+			//remove data from list
+			list.removeChild(li);
+			//remove data from local storage
+			localStorage.removeItem(key);
+		}
+	}
 }
+
+
