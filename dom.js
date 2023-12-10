1418d7f8bc96c5d3dca8010f4a0009dac2caf8b7
Atif-1 <atif1712001@gmail.com>
2023-12-10 19:17:16 +0530

    Task 11

--- dom.js
+++ dom.js
@@ -1,13 +1,16 @@
 var form=document.querySelector('#main');
 
 form.addEventListener('submit',onSubmit);
-
 function onSubmit(e){
-let n=document.querySelector('#name').value;
-let email=document.querySelector('#mail').value;
-let p=document.querySelector('#phone').value;
-localStorage.setItem('name',n);
-localStorage.setItem('email',email);
-localStorage.setItem('phone',p);
-
- }
+	e.preventDefault();
+// let n=document.querySelector('#name').value;
+// let email=document.querySelector('#mail').value;
+// let p=document.querySelector('#phone').value;
+const my_obj=new Object();
+my_obj.name=document.querySelector('#name').value;
+my_obj.email=document.querySelector('#mail').value;
+my_obj.phone=document.querySelector('#phone').value;
+	const objJSON=JSON.stringify(my_obj);
+	localStorage.setItem(serial,objJSON);
+	
+}
